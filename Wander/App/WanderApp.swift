814f53879afa77f710541a1827d67de6ef6b44ec
Joe Lipshutz <joelipshutz@Manjunath-s-Galaxy-M11.localdomain>
2026-09-21 10:13:43 -0700

    Use contact fixtures throughout main-screen UI tests

--- Wander/App/WanderApp.swift
+++ Wander/App/WanderApp.swift
@@ -181,7 +181,9 @@ struct WanderApp: App {
             )
         )
         #if DEBUG
-        _mapCaptureBackend = StateObject(wrappedValue: Self.makeMapCaptureBackend())
+        _mapCaptureBackend = StateObject(wrappedValue:
+            usesSimulatorTestSession && ProcessInfo.processInfo.arguments.contains("-WanderContactDiscoveryUITest")
+                ? backendStore : Self.makeMapCaptureBackend())
         #endif
     }
 
